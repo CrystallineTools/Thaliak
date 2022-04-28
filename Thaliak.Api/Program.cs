9801abad870d940cb3978c7e7177422de23dfac3
Ava Chaney <hello@ava.dev>
2022-04-27 21:11:06 -0700

    use OAS in prod, too

--- Thaliak.Api/Program.cs
+++ Thaliak.Api/Program.cs
@@ -13,12 +13,8 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
