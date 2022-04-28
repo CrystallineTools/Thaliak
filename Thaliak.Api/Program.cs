1590511079e25fe070ca14e3ff8d7c89635bb107
Ava Chaney <hello@ava.dev>
2022-04-27 21:02:08 -0700

    add workflow to build api

--- Thaliak.Api/Program.cs
+++ Thaliak.Api/Program.cs
@@ -8,7 +8,6 @@ builder.Services.AddDbContext<ThaliakContext>(o => o.UseNpgsql(builder.Configura
 builder.Services.AddAutoMapper(typeof(ThaliakMapperProfile));
 builder.Services.AddControllers();
 
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
