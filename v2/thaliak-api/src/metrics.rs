365728c7150c7889ba0d775dab0a7260dd7ab875
Ava Chaney <hello@ava.dev>
2025-11-13 01:07:05 -0800

    v2(api): initialize histogram buckets properly

--- v2/thaliak-api/src/metrics.rs
+++ v2/thaliak-api/src/metrics.rs
@@ -13,6 +13,22 @@ pub async fn init_metrics_exporter() -> eyre::Result<()> {
     let addr = SocketAddr::from(([0, 0, 0, 0], 9090));
     PrometheusBuilder::new()
         .with_http_listener(addr)
+        .set_buckets_for_metric(
+            metrics_exporter_prometheus::Matcher::Full(
+                "thaliak_http_request_duration_seconds".to_string(),
+            ),
+            &[
+                0.001, 0.005, 0.01, 0.025, 0.05, 0.1, 0.25, 0.5, 1.0, 2.5, 5.0, 10.0,
+            ],
+        )?
+        .set_buckets_for_metric(
+            metrics_exporter_prometheus::Matcher::Full(
+                "thaliak_db_query_duration_seconds".to_string(),
+            ),
+            &[
+                0.0001, 0.0005, 0.001, 0.0025, 0.005, 0.01, 0.025, 0.05, 0.1, 0.25, 0.5, 1.0,
+            ],
+        )?
         .install()?;
 
     info!("starting Prometheus metrics server at http://{}", addr);
